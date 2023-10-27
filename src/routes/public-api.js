be813b96729bd9ea71a77305a7b7cfae1f602293
fadhil <fadhil@technopartner.co.id>
2023-10-27 14:05:44 +0700

    updated

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -2,6 +2,7 @@ import express from 'express'
 import userController from '../controllers/user-controller.js'
 
 const publicRouter = new express.Router()
+publicRouter.get('/', (req, res) => {res.send("Express on Vercel")})
 publicRouter.post('/api/users', userController.register)
 publicRouter.post('/api/users/login', userController.login)
 
