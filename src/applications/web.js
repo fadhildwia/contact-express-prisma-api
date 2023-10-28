842a3b20d9752c979710e5eab13b64d8104627db
Fadhil <fadhilahdwi.a@gmail.com>
2023-10-29 00:41:39 +0700

    updated

--- src/applications/web.js
+++ src/applications/web.js
@@ -1,12 +1,12 @@
 import express from "express";
-// import { publicRouter } from "../routes/public-api.js";
+import { publicRouter } from "../routes/public-api.js";
 import { errorMiddleware } from "../middlewares/error-middleware.js";
 import { userRouter } from "../routes/user-api.js";
 
 export const web = express();
 web.use(express.json());
 
-// web.use(publicRouter);
+web.use(publicRouter);
 web.use(userRouter);
 
 web.use(errorMiddleware);
